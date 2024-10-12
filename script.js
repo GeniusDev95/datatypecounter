7953350483c03c25660b1ba35e3a20801f0694ec
Shahbaz Khatri <shahbaz.djarian@gmail.com>
2024-10-12 11:47:07 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+var arr = ["2", 2, "a", 3, 6, true, false, [1, "2", [1, "a", "b", true, false], "a"], false, true];
+
+var countOfStr = 0
+var countOfNum = 0
+var countOfBool = 0
+var countOfObj = 0
+
+let flatArray = arr.flat(Infinity);
+
+for(i=0; i<(flatArray.length*2);i++){
+    if(typeof(arr[i]) === "object"){
+        countOfObj++    
+        arr.push(...arr[i]);
+    }
+}
+
+for(i=0; i<flatArray.length; i++){    
+    if(typeof(flatArray[i]) === "string"){
+        countOfStr++
+    }
+    else if(typeof(flatArray[i]) === "number"){
+        countOfNum++
+    } 
+    else if(typeof(flatArray[i]) === "boolean"){
+        countOfBool++
+    }
+}
+
+console.log("Total number of strings =", countOfStr,"\n","Total number of Numbers =", countOfNum, "\n","Total number of Booleans =", countOfBool, "\n","Total number of Nested Arrays =", countOfObj)
